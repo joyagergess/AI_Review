17a4634ea0c819ee985dd287418bbd387800d15b
joyagergess <gergessjoya@gmail.com>
2025-11-08 11:15:50 +0200

    Added axios to connect frontend to backend , and used it in js to send data as json

--- Frontend/Script/main.js
+++ Frontend/Script/main.js
@@ -3,9 +3,19 @@
 document.getElementById("review_button").addEventListener('click',async ()=>{
  const file = document.getElementById('filename').value.trim();
  const code = document.getElementById('code').value;
-
  if (!file|| !code) {
     return alert("Enter filename and code!");
  }
-document.getElementById('output').textContent= file + code;
-});
+
+ axios.post("http://localhost/AI_Review/Backend/review.php",{
+              Filename: file ,Code :code
+              })
+               .then(response =>{
+                console.log("Server response:", response.data);
+               })
+                .catch(error =>{
+                  console.error("Error",error)
+                });
+               });
+
+         
