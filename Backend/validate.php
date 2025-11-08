0b6290123cdd59ae45707d0843aa468cef573f21
joyagergess <gergessjoya@gmail.com>
2025-11-08 20:57:56 +0200

    Going through the reviews array to validate them

---
+++ Backend/validate.php
@@ -0,0 +1,10 @@
+<?php
+function validate(array $reviewArray): array {
+    $Severities = ['low', 'medium', 'high'];
+    $validated = [];
+
+    foreach ($reviewArray as $item) {
+        
+  
+    }
+}
